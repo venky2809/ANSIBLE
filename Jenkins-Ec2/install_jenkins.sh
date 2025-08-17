c1b7a7dab943d2a9f479bf3e1f7a55e571c21b08
Ubuntu <ubuntu@ip-172-31-27-25.eu-west-2.compute.internal>
2025-08-17 19:31:04 +0000

    changed

--- Jenkins-Ec2/install_jenkins.sh
+++ Jenkins-Ec2/install_jenkins.sh
@@ -6,7 +6,6 @@ sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 sudo apt install git -y  
-cd /home/ubuntu
 git clone https://github.com/venky2809/ANSIBLE.git
 cd ANSIBLE
 ansible-playbook -i /home/ubuntu/ANSIBLE/Jenkins-playbook.yml
