95226b4ad2a08724ed4e1f514e08a01b35f2f2f4
Ubuntu <ubuntu@ip-172-31-27-25.eu-west-2.compute.internal>
2025-08-17 19:17:29 +0000

    changed

--- Jenkins-Ec2/install_jenkins.sh
+++ Jenkins-Ec2/install_jenkins.sh
@@ -5,9 +5,7 @@ sudo apt update -y
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
-sudo apt install git -y 
-mkdir Ansible && cd Ansible
-pwd
+sudo apt install git -y  
 cd /home/ubuntu
 git clone https://github.com/venky2809/ANSIBLE.git
 cd ANSIBLE
